3a9450ee0abf02ff9a148b9a1e48696df059618b
Omuganda863 <omugandadickson@gmail.com>
2023-09-29 14:22:59 +0300

    Villain Story

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GamePrototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Skynet";
+
+            Console.WriteLine("My Name is XT-4000, an AI from the Future Programmed To Destroy Mankind\nWhat is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello "+name+" What is your favourite Colour");
+            string colour = Console.ReadLine();
+
+            Console.WriteLine("Your favourite colour is "+colour+", Weak, Mine is Destruction!");
+
+            Console.ReadKey();
+        }
+    }
+}
