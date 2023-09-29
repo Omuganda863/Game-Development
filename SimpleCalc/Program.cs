f329a0582bb145b3c460fc250d5c1d0e746ef8bb
Omuganda863 <omugandadickson@gmail.com>
2023-09-29 19:26:11 +0300

    Simple Addition

---
+++ SimpleCalc/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SimpleCalc;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num1, num2 , result;
+        Console.Write("Enter First number: ");
+        num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter Second number: ");
+        num2 = Convert.ToInt32(Console.ReadLine());
+        result = num1 + num2;
+        Console.WriteLine("The Result is: \n" + result);
+
+    }
+}
