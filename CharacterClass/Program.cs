e5c3ddc58003a1bf16b2aa92ee780cb2b3ec676b
Omuganda863 <omugandadickson@gmail.com>
2023-10-03 06:48:10 +0300

    classes

---
+++ CharacterClass/Program.cs
@@ -0,0 +1,36 @@
+﻿
+namespace CharacterClass;
+class Alien
+{
+public string name;
+public string sector;
+public string abilities;
+public int xp = 0;
+
+public void powers(){
+Console.WriteLine("My name is "+ name +" from the " + sector);
+Console.WriteLine("Witness the the beauty of destruction!");
+Console.WriteLine(name + " Uses "+ abilities);
+xp ++;
+
+}
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    Alien alien1 = new Alien ();
+    alien1.name = "Xenomorph";
+    alien1.sector = "Promethian Sector";
+    alien1.abilities = "Telekinesis";
+
+     alien1.powers ();
+
+    Console.ReadKey();
+
+    
+    }
+}
